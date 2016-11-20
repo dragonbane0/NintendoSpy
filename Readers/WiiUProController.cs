555b295f0ee2924509ff3a88b2088b5a247e77d4
dragonbane <marwinmisselhorn@hotmail.de>
2016-11-20 11:27:17 +0100

    Finished up the WiiU Gamepad

--- Readers/WiiUProController.cs
+++ Readers/WiiUProController.cs
@@ -109,8 +109,7 @@ namespace NintendoSpy.Readers
             try
             {
                 float StickX, StickCX, StickY, StickCY = 0.0f;
-                byte Directions1, Directions2 = 0;
-                byte Buttons1, Buttons2 = 0;
+                byte Buttons1, Buttons2, Buttons3 = 0;
 
                 while (true)
                 {
@@ -120,47 +119,12 @@ namespace NintendoSpy.Readers
                         case 0x01: //Input Data
                             {
                                 //1 = 0 bit, 2 = 1 bit, 4 = 2 bit, 8 = 3 bit, 16 = 4 bit, 32 = 5 bit, 64 = 6 bit, 128 = 7 bit        
-                                              
-                                /*
-                                A is 80
-                                B is 40
-                                Y is 10
-                                X is 20
-                                R is 0
-                                */
 
-                                StickX = reader.ReadSingle();
-                                StickY = reader.ReadSingle();
-                                StickCX = reader.ReadSingle();
-                                StickCY = reader.ReadSingle();
-
-                                Directions1 = reader.ReadByte();
-                                Directions2 = reader.ReadByte();
-                                Buttons1 = reader.ReadByte();
-                                Buttons2 = reader.ReadByte();
-        
                                 //Apply Inputs
                                 var outState = new ControllerStateBuilder();
 
-                                outState.SetButton("a", IsBitSet(Buttons1, 7));
-                                outState.SetButton("b", IsBitSet(Buttons1, 6));
-                                outState.SetButton("x", IsBitSet(Buttons1, 5));
-                                outState.SetButton("y", IsBitSet(Buttons1, 4));
-
-                                outState.SetButton("home", IsBitSet(Buttons2, 5)); //l
-                                outState.SetButton("touch", IsBitSet(Buttons2, 4)); //r
-                                outState.SetButton("zl", IsBitSet(Buttons2, 7));
-                                outState.SetButton("zr", IsBitSet(Buttons2, 6));
-                                //outState.SetButton("touch", IsBitSet(Buttons2, 6));
-
-                                outState.SetButton("plus", IsBitSet(Buttons2, 3));
-                                outState.SetButton("minus", IsBitSet(Buttons2, 2));
-                                //outState.SetButton("home", IsBitSet(Buttons2, 2));
-
-                                outState.SetButton("up", IsBitSet(Buttons1, 1));
-                                outState.SetButton("down", IsBitSet(Buttons1, 0));
-                                outState.SetButton("left", IsBitSet(Buttons1, 3));
-                                outState.SetButton("right", IsBitSet(Buttons1, 2));
+                                StickX = reader.ReadSingle();
+                                StickY = reader.ReadSingle();
 
                                 if (StickX <= 1.0f && StickY <= 1.0f && StickX >= -1.0f && StickY >= -1.0f)
                                 {
@@ -168,17 +132,41 @@ namespace NintendoSpy.Readers
                                     outState.SetAnalog("lstick_y", StickY);
                                 }
 
+                                StickCX = reader.ReadSingle();
+                                StickCY = reader.ReadSingle();
+
                                 if (StickCX <= 1.0f && StickCY <= 1.0f && StickCX >= -1.0f && StickCY >= -1.0f)
                                 {
                                     outState.SetAnalog("cstick_x", StickCX);
                                     outState.SetAnalog("cstick_y", StickCY);
                                 }
 
-                                if (Directions2 >= 0 && Directions2 <= 255)
-                                {
-                                    outState.SetButton("l3", IsBitSet(Directions2, 2)); //!!Analog Stick Click is 4; C-Stick is 2!!
-                                    outState.SetButton("r3", IsBitSet(Directions2, 1));
-                                }
+                                Buttons1 = reader.ReadByte();
+                                Buttons2 = reader.ReadByte();
+                                Buttons3 = reader.ReadByte();
+
+                                outState.SetButton("a", IsBitSet(Buttons1, 7));
+                                outState.SetButton("b", IsBitSet(Buttons1, 6));
+                                outState.SetButton("x", IsBitSet(Buttons1, 5));
+                                outState.SetButton("y", IsBitSet(Buttons1, 4));
+
+                                outState.SetButton("left", IsBitSet(Buttons1, 3));
+                                outState.SetButton("right", IsBitSet(Buttons1, 2));
+                                outState.SetButton("up", IsBitSet(Buttons1, 1));
+                                outState.SetButton("down", IsBitSet(Buttons1, 0));
+
+                                outState.SetButton("zl", IsBitSet(Buttons2, 7));
+                                outState.SetButton("zr", IsBitSet(Buttons2, 6));
+                                outState.SetButton("l", IsBitSet(Buttons2, 5)); 
+                                outState.SetButton("r", IsBitSet(Buttons2, 4));
+
+                                outState.SetButton("plus", IsBitSet(Buttons2, 3));
+                                outState.SetButton("minus", IsBitSet(Buttons2, 2));
+                                outState.SetButton("l3", IsBitSet(Buttons2, 1));
+                                outState.SetButton("r3", IsBitSet(Buttons2, 0));
+
+                                outState.SetButton("home", IsBitSet(Buttons3, 1));
+                                outState.SetButton("touch", IsBitSet(Buttons3, 2));
 
                                 if (ControllerStateChanged != null)
                                     _context.Post((SendOrPostCallback)(_ => ControllerStateChanged(this, outState.Build())), null);
